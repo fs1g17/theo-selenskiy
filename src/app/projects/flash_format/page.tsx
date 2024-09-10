4616e20b7eec4186dfbd0a31866e45a96863e200
fs1g17 <fs1g17@soton.ac.uk>
2024-09-10 15:21:10 +0100

    feat(flashformat): write a demo about the project

--- src/app/projects/flash_format/page.tsx
+++ src/app/projects/flash_format/page.tsx
@@ -1,5 +1,50 @@
+import Link from "next/link";
+
 export default function FlashFormat() {
-  return(
-    <div>flash_format</div>
-  )
+  return (
+    <div className="w-full flex flex-col gap-y-10 pb-10">
+      <div className="flex flex-col">
+        <h1>Flash Format</h1>
+        <Link
+          href="https://flash-format-git-main-aquilex.vercel.app/"
+          target="_blank"
+          className="text-muted-foreground"
+        >
+          Check it out! (opens in new tab)
+        </Link>
+      </div>
+      <div>
+        <h2>Demo</h2>
+        <video
+          controls
+          src={"/videos/flashformat-demo.mp4"}
+          autoPlay
+          muted
+          className="w-full aspect-auto"
+        />
+      </div>
+      <div>
+        <h2>What problem does it solve?</h2>
+        <p className="text-muted-foreground">
+          Converting images to different formats requires tools to be downloaded
+          on the computer, but that's inconvenient if you just need to convert a
+          single file quickly.
+        </p>
+      </div>
+      <div>
+        <h2>What does it do?</h2>
+        <p className="text-muted-foreground">
+          This small web app allows the user to convert images to different
+          formats online, in a flash! Hence the name, flash format.
+        </p>
+      </div>
+      <div>
+        <h2>Tech approach</h2>
+        <p className="text-muted-foreground">
+          The frontend is implemented with NextJs. The file conversion is done
+          with a firebase cloud function, using the 'sharp' library.
+        </p>
+      </div>
+    </div>
+  );
 }
--- src/app/projects/mercante/page.tsx
+++ src/app/projects/mercante/page.tsx
@@ -1,5 +1,44 @@
 export default function Mercante() {
   return(
-    <div>mercante</div>
+    <div className="w-full flex flex-col gap-y-10 pb-10">
+    <div className="flex flex-col">
+      <h1></h1>
+      <Link
+        href=""
+        target="_blank"
+        className="text-muted-foreground"
+      >
+        Check it out! (opens in new tab)
+      </Link>
+    </div>
+    <div>
+      <h2>Demo</h2>
+      <video
+        controls
+        src={"/videos/anymate-demo.mp4"}
+        autoPlay
+        muted
+        className="w-full aspect-auto"
+      />
+    </div>
+    <div>
+      <h2>What problem does it solve?</h2>
+      <p className="text-muted-foreground">
+        
+      </p>
+    </div>
+    <div>
+      <h2>What does it do?</h2>
+      <p className="text-muted-foreground">
+        
+      </p>
+    </div>
+    <div>
+      <h2>Tech approach</h2>
+      <p className="text-muted-foreground">
+        
+      </p>
+    </div>
+  </div>
   )
 }
